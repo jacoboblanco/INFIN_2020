ab382d88c0dcc69e705b5e6130aa7cda86041908
carlos1491875 <62296252+carlos1491875@users.noreply.github.com>
2020-03-20 23:56:05 +0100

    Update cola_circular.c

--- carlos/cola_circular.c
+++ carlos/cola_circular.c
@@ -1,36 +1,65 @@
 
 
 
+
+
+
+
+
 //cola circular
 
 #include <stdio.h>
 
 int main () {
-	// entrada del dato
-	int frente=0;
-	int tamaño=9;
-	int n =0;
-	int dades[tamaño]; 	
-	printf("introduzca el dato en la cola\n");
-	scanf("%i", dato);
-
+   
+   //el problema a solucionar es que cuando no hay valores en la lista 
+   //printea las direcciones...
 	
-	if (n==tamaño){
-		printf("la cola está llena");
+	int dades[8];  //vector almacen
+    	int ubi=0;     //posicion del vector que se printea
+	int s=1;       //STOP
+	int S=1;       //STOP
+	int d;         //dato que le llega del cliente
+	
+	int f=0;       //frente
+	int t=7;       //tamaño
+	int n =0;      //numero de datos
+	
+	int o=0;       //output
+//recepcion del dato
+
+
+while (s==1){
+    printf("introduzaca el valor\n");
+    scanf("%i",&d);                       
+	printf("\n\n");
+			                              
+			 ubi=(f+n)%t;   //ubi=posicion del vector rellenamos
+			 dades[ubi]=d;  //dades[0,1,2,3,4,5,6]
+			 n++;
+			//printf("%i\n",ubi);
+			
+			printf("%d\n",dades[0]);
+			printf("%d\n",dades[1]);
+			printf("%d\n",dades[2]);
+			printf("%d\n",dades[3]);
+			printf("%d\n",dades[4]);
+			printf("%d\n",dades[5]);
+			printf("%d\n",dades[6]);
+			
+		} 
+		
+		
+		//salida del dato
+	
+	if(S==1){
+			o=dades[f];
+			printf("la temperatura es %i",o);
+			f=(f+1)%t;
+			n--;
 		}
-		else {	
-			int ubi=(frente+n)%tamaño;
-			dades[ubi]=dato;
-			n++;
-	 }	
-	//salida del dato
-	int salida= dades[frente];
-	printf("la temperatura es: %i",salida);
-	(frente=frente+1)%tamaño;
-	n--;
-	
-	
-
+			
+		
 return 0;
 }
 

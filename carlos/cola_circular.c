7d99e8bf095677364d1743fd9f4169d04560d724
carlos1491875 <62296252+carlos1491875@users.noreply.github.com>
2020-03-22 13:43:22 +0100

    Update cola_circular.c

--- carlos/cola_circular.c
+++ carlos/cola_circular.c
@@ -1,66 +1,91 @@
-
-
-
-
-
-
-
-
 //cola circular
 
 #include <stdio.h>
+void recepcion ();
+void envia ();
+void max ();
+void min ();
 
-int main () {
-   
-   //el problema a solucionar es que cuando no hay valores en la lista 
-   //printea las direcciones...
-	
 	int dades[8];  //vector almacen
-    	int ubi=0;     //posicion del vector que se printea
-	int s=1;       //STOP
+    int ubi=0;     //posicion del vector que se printea
+	int s=11;       //STOP
 	int S=1;       //STOP
 	int d;         //dato que le llega del cliente
-	
 	int f=0;       //frente
 	int t=7;       //tamaño
 	int n =0;      //numero de datos
-	
 	int o=0;       //output
-//recepcion del dato
+    int v;
+    int mayor=0;
+	int menor=100000000;
 
-
-while (s==1){
-    printf("introduzaca el valor\n");
-    scanf("%i",&d);                       
-	printf("\n\n");
-			                              
-			 ubi=(f+n)%t;   //ubi=posicion del vector rellenamos
-			 dades[ubi]=d;  //dades[0,1,2,3,4,5,6]
-			 n++;
-			//printf("%i\n",ubi);
-			
-			printf("%d\n",dades[0]);
-			printf("%d\n",dades[1]);
-			printf("%d\n",dades[2]);
-			printf("%d\n",dades[3]);
-			printf("%d\n",dades[4]);
-			printf("%d\n",dades[5]);
-			printf("%d\n",dades[6]);
-			
-		} 
-		
-		
-		//salida del dato
-	
-	if(S==1){
-			o=dades[f];
-			printf("la temperatura es %i",o);
-			f=(f+1)%t;
-			n--;
-		}
-			
-		
+int main () {
+    recepcion();
+	envia();
+	max();
+    min();
 return 0;
 }
+
+void recepcion (){
+	printf ("pulse 69 para activar la adquisicion");
+	scanf("%i",&s);
+	while (s==69){
+		printf("introduzaca el valor\n");
+		scanf("%i",&d);                       
+		printf("\n\n");
+											  
+				 ubi=(f+n)%t;   //ubi=posicion del vector rellenamos
+				 dades[ubi]=d;  //dades[0,1,2,3,4,5,6]
+				 n++;
+				int i;
+				for (i=0;i<=ubi;i++){
+					printf("%d\n",dades[i]);
+					}
+				
+			} 
+			
+}
+
+void envia (){	
+	printf("Introduzca el numero de datos a sacar:");
+    scanf("%i",&v);
+	int i;
+	for (i=0; i<v; i++){
+		
+	  if(S==1){
+				o=dades[f];
+				printf("la temperatura es %d\n",o);
+				f=(f+1)%t;
+				n--;
+		}
+	}
+}
+void max (){
+
+    for (int i=0;i<t;i++){
+		if (dades[i]>mayor){
+			mayor=dades[i];
+		}
+	}
+	printf("El valor mayor es: %i\n",mayor);
+}
+
+void min (){
+
+    for (int i=0;i<t;i++){
+		if (dades[i]<menor){
+			menor=dades[i];
+		}
+	}
+	printf("El valor mayor es: %i\n",menor);
+}
+
+
+
+
+
+
+
 
 

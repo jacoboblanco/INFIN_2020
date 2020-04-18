2fd6caf9b4ee6d53bf56c73ab783f7ed010b46c0
aleixnogues <62295239+aleixnogues@users.noreply.github.com>
2020-04-18 13:17:32 +0200

    Update Ard1.ino

--- control_sensor/Ard1.ino
+++ control_sensor/Ard1.ino
@@ -1,100 +1,53 @@
-char entrada[8]; //array de entrada
-char salida [8]; //array de salida
-
 char comanda;
+char entrada[8]; //array de entrada
+int posicion=0;
 
-int posicion=0; //posición de lectura array entrada
-int PM=0; //PM=0 Paro, PM=1 Marcha
-int pm;
-int v; //parametro v array entrada
-float mostreo; //freq. de mostreo del sensor
-
-void setup() {
+void setup(){
   Serial.begin(9600);
 }
 
-
-void loop() {
+void loop(){
   lectura_array();
+  delay(20);
+  operacion();
+  delay(500);
+  
+}
+
+void operacion() {
+
+  switch (comanda){
+		case 'M':
+			Serial.println ("S'executa la comanda M");
+		break;
+    
+    	case 'S':
+			Serial.println ("S'executa la comanda S");
+		break;
+    
+    	case 'E':
+			Serial.println ("S'executa la comanda E");
+		break;
+    
+    	case 'C':
+			Serial.println ("S'executa la comanda C");
+		break;
+
+		default:
+			Serial.println ("Error");
+		break;
+  }
 }
 
 
 void lectura_array() {
  
-    if(Serial.available()>0) //Comprobamos si en el buffer hay datos
+    while (Serial.available()>0) //Comprobamos si en el buffer hay datos
     {
-    char entrada=Serial.read();  //Lee cada carácter uno por uno y se almacena en una variable
- 	Serial.print(entrada);  //Imprimimos en la consola el carácter recibido
+    entrada[posicion]=Serial.read();  //Lee cada carácter uno por uno y se almacena en una variable
+    posicion++;
     }
+  Serial.println(entrada);  //Imprimimos en la consola el carácter recibido
+  comanda=entrada[1];
+  Serial.println(comanda);
 }
-
-void array_salida() {
-   salida[0] = 'A';
-    salida[1] = comanda;
-}
-
-/*void operacion() {
-  
-  switch (entrada[1]) {
-    case 'M':
-      comanda = 'M';
-      parada_marxa();
-      break;
-    
-    case 'S':
-      comanda = 'S';
-      sort_digital();
-      break;
-    
-    case 'E':
-      comanda = 'E';
-      ent_digital();
-      break;
-    
-    case 'C':
-      comanda = 'C';
-      ultima_m();
-      break;
-  }
-}
-
-void parada_marxa() {
-  
-  pm = entrada[2];
-  
-  switch (PM) {
-    
-    case 0:
-     if (v=0) {
-      errorparametros();
-      }
-    else if (v=1) {
-      PM = 1;
-      //mostreo = 
-      
-      
-      
-    break;
-    
-    case 1:
-    
-    break;
-  }
-}
-  
-  void sort_digital() {
-  }
-  
-  void ent_digital() {
-  }
-  
-  void ultima_m() {
-  }
-  
-  void parada_marxa() {
-  }
-  
-  void errorparametros() {
-  }
-  
-  */
